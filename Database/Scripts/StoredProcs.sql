30ca5329edcb331ea0cc9bccea5e17023b84c6d9
LeoMrozek1895 <63420482+LeoMrozek1895@users.noreply.github.com>
2020-07-22 10:14:03 -0500

    Add files via upload

--- Database/Scripts/StoredProcs.sql
+++ Database/Scripts/StoredProcs.sql
@@ -1,27 +1,39 @@
 USE [DNN-Covid19]
 GO
 
-/****** Object:  StoredProcedure [dbo].[usp_Covid19_RowData]    Script Date: 7/17/2020 11:26:07 AM ******/
+/****** Object:  StoredProcedure [dbo].[usp_LoadCovid19ActivityTable]    Script Date: 7/22/2020 10:12:24 AM ******/
+DROP PROCEDURE [dbo].[usp_LoadCovid19ActivityTable]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_Covid19_TruncateData]    Script Date: 7/22/2020 10:12:24 AM ******/
+DROP PROCEDURE [dbo].[usp_Covid19_TruncateData]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_Covid19_RowData]    Script Date: 7/22/2020 10:12:24 AM ******/
+DROP PROCEDURE [dbo].[usp_Covid19_RowData]
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_Covid19_RowData]    Script Date: 7/22/2020 10:12:24 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[usp_Covid19_RowData]
+CREATE PROCEDURE [dbo].[usp_Covid19_RowData]
 AS
 SELECT COUNT(*) as "RowCount", MAX([REPORT_DATE]) as LastDataDate 
 FROM [dbo].[Covid19-Activity]
 GO
 
-/****** Object:  StoredProcedure [dbo].[usp_Covid19_TruncateData]    Script Date: 7/17/2020 11:26:07 AM ******/
+/****** Object:  StoredProcedure [dbo].[usp_Covid19_TruncateData]    Script Date: 7/22/2020 10:12:24 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE OR ALTER PROCEDURE [dbo].[usp_Covid19_TruncateData]
+CREATE PROCEDURE [dbo].[usp_Covid19_TruncateData]
 	@Id		int = 0
 AS
 IF @ID = 0 BEGIN
@@ -34,7 +46,7 @@ END
 
 GO
 
-/****** Object:  StoredProcedure [dbo].[usp_LoadCovid19ActivityTable]    Script Date: 7/17/2020 11:26:07 AM ******/
+/****** Object:  StoredProcedure [dbo].[usp_LoadCovid19ActivityTable]    Script Date: 7/22/2020 10:12:24 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -43,12 +55,25 @@ GO
 
 
 
-CREATE OR ALTER PROCEDURE [dbo].[usp_LoadCovid19ActivityTable]
+CREATE PROCEDURE [dbo].[usp_LoadCovid19ActivityTable]
 AS
 
+--TRUNCATE TABLE [dbo].[Covid19-Activity-Import]
 INSERT INTO [dbo].[Covid19-Activity] ([PEOPLE_POSITIVE_CASES_COUNT], [COUNTY_NAME], [REPORT_DATE], [PROVINCE_STATE_NAME], [CONTINENT_NAME], [DATA_SOURCE_NAME], [PEOPLE_DEATH_NEW_COUNT], [COUNTY_FIPS_NUMBER], [COUNTRY_ALPHA_3_CODE], [COUNTRY_SHORT_NAME], [COUNTRY_ALPHA_2_CODE], [PEOPLE_POSITIVE_NEW_CASES_COUNT], [PEOPLE_DEATH_COUNT])
-SELECT CONVERT(bigint, [PEOPLE_POSITIVE_CASES_COUNT]), [COUNTY_NAME], CONVERT(date, [REPORT_DATE]), [PROVINCE_STATE_NAME], [CONTINENT_NAME], [DATA_SOURCE_NAME], CONVERT(bigint, [PEOPLE_DEATH_NEW_COUNT])
-	, [COUNTY_FIPS_NUMBER], [COUNTRY_ALPHA_3_CODE], [COUNTRY_SHORT_NAME], [COUNTRY_ALPHA_2_CODE], CONVERT(bigint, [PEOPLE_POSITIVE_NEW_CASES_COUNT]), CONVErT(bigint, [PEOPLE_DEATH_COUNT])
+SELECT 
+	  CONVERT(bigint, CONVERT(numeric, [PEOPLE_POSITIVE_CASES_COUNT]))AS [PEOPLE_POSITIVE_CASES_COUNT]
+	, [COUNTY_NAME]
+	, CONVERT(date, [REPORT_DATE])
+	, [PROVINCE_STATE_NAME]
+	, [CONTINENT_NAME]
+	, [DATA_SOURCE_NAME]
+	, CONVERT(bigint, CONVERT(numeric, [PEOPLE_DEATH_NEW_COUNT]))AS [PEOPLE_DEATH_NEW_COUNT]
+	, [COUNTY_FIPS_NUMBER]
+	, [COUNTRY_ALPHA_3_CODE]
+	, [COUNTRY_SHORT_NAME]
+	, [COUNTRY_ALPHA_2_CODE]
+	, CONVERT(bigint, CONVERT(numeric, [PEOPLE_POSITIVE_NEW_CASES_COUNT]))AS [PEOPLE_POSITIVE_NEW_CASES_COUNT]
+	, CONVERT(bigint, CONVERT(numeric, [PEOPLE_DEATH_COUNT]))AS [PEOPLE_DEATH_COUNT]
 FROM [dbo].[Covid19-Activity-Import]
 
 DECLARE @ImportCount int
@@ -60,6 +85,6 @@ SELECT @CopyCount = COUNT(*) FROM [dbo].[Covid19-Activity]
 SELECT @ImportCount as ImportCount, @CopyCount as CopyCount
 
 ALTER INDEX ALL ON [Covid19-Activity] REBUILD
-GO
 
+GO
 
